db9770f7481b17b3fc1ea86980e5fa6d14b0545d
canutog828-glitch <canutog828@gmail.com>
2026-07-20 14:44:36 -0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "cagalhão"
+rootProject.name = "TestPlugins"
 
 // This file sets what projects are included.
 // All new projects should get automatically included unless specified in the "disabled" variable.
