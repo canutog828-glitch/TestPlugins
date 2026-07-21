4862b9a25840a4752041ff86f181ee823b7eb617
canutog828-glitch <canutog828@gmail.com>
2026-07-21 13:08:14 -0400

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import android.util.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.network.CloudflareKiller
@@ -207,3 +208,4 @@ for (api in apis) {
 }
 
 return foundAny
+    }
