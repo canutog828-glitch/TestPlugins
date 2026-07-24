a1761749ad5e476facaa34cd8020a0fece37ce27
canutog828-glitch <canutog828@gmail.com>
2026-07-24 14:44:26 -0400

    Update plugin metadata in build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -7,27 +7,17 @@ dependencies {
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
+    description = "Plugin para assistir filmes, séries, animes e doramas"
+    authors = listOf("lospobreflix")
 
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
+    status = 1 // 0=Down, 1=Ok, 2=Slow, 3=Beta
 
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
+    tvTypes = listOf("Movie", "TvSeries", "Anime")
 
     requiresResources = true
-    language = "en"
+    language = "pt"
 
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+    iconUrl = "https://lospobreflix.lat/favicon.ico"
 }
 
 android {
