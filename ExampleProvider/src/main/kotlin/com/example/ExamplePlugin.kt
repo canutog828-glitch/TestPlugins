c24c51379eda4973d90fcb50304c187839de2783
canutog828-glitch <canutog828@gmail.com>
2026-07-24 09:08:38 -0400

    Clean up ExamplePlugin by removing activity reference
    Removed unused activity variable and related code.

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -4,22 +4,12 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.lospobreflix.ExampleProvider
 
 @CloudstreamPlugin
 class ExamplePlugin: Plugin() {
-    private var activity: AppCompatActivity? = null
-
     override fun load(context: Context) {
-        activity = context as? AppCompatActivity
-
         // All providers should be added in this manner
         registerMainAPI(ExampleProvider())
-
-        openSettings = {
-            val frag = BlankFragment(this)
-            activity?.let {
-                frag.show(it.supportFragmentManager, "Frag")
-            }
-        }
     }
 }
